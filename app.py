2a9fef5b03cd21b8624f5375c78c9f491191a25b
Monish Nallagondalla <nsmonish@gmail.com>
2025-05-20 22:01:10 +0530

    arxiv wiki DuckDuckGoSearch apicall

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ from langchain.agents import initialize_agent,AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
+
+
+## Arxiv and wikipedia Tools
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
+
+search=DuckDuckGoSearchRun(name="Search")
+
+
+st.title("🔎 LangChain - Chat with search")
